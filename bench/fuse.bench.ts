e853be0bd31d2b0b184f655ccad0b92a2a411209
Akshay Paka <122613966+akshay-xp@users.noreply.github.com>
2026-07-27 20:37:07 +0530

    chore(fuse): bench fuse16 alongside fuse8

--- bench/fuse.bench.ts
+++ bench/fuse.bench.ts
@@ -1,12 +1,17 @@
 import { bench, run } from "mitata";
 
-import { BinaryFuse8 } from "../src/fuse/fuse.js";
+import { BinaryFuse8, BinaryFuse16 } from "../src/fuse/fuse.js";
 
 const keys = Array.from({ length: 100000 }, (_, i) => `key:${String(i)}`);
 const f8 = BinaryFuse8.from(keys);
+const f16 = BinaryFuse16.from(keys);
 
 bench("fuse8 build", () => BinaryFuse8.from(keys));
 bench("fuse8 has (hit)", () => f8.has("key:1"));
 bench("fuse8 has (miss)", () => f8.has("absent"));
+
+bench("fuse16 build", () => BinaryFuse16.from(keys));
+bench("fuse16 has (hit)", () => f16.has("key:1"));
+bench("fuse16 has (miss)", () => f16.has("absent"));
 
 await run();
